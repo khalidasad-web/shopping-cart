d020f236710b60cdfcffa9eee604ae5eee34e327
khalidasad-web <khalid.ibnal.asad@gmail.com>
2021-02-21 10:33:32 +0000

    5th-Commit-21Feb2021

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-function updateItems(itemQty, itemPrice) {
-    let tempPrice = document.getElementById(itemPrice).innerText.split('$');
-    tempPrice.shift();
-    const unitPrice = parseFloat(tempPrice);
+function updateItems(itemQty, itemPriceTag, itemPrice, action) {
+    let currentPrice = itemPrice;
     let currentItemQty = parseInt(document.getElementById(itemQty).value);
-    console.log(unitPrice, currentItemQty);
-
+    switch (action) {
+        case 'minus':
+            if (currentItemQty > 1) {
+                currentItemQty--;
+                currentPrice = currentItemQty * currentPrice;
+            } else {
+                console.log('Minimum quatity reached', currentItemQty);
+            }
+            break;
+        case 'plus':
+            currentItemQty++;
+            currentPrice = currentItemQty * currentPrice;
+            break;
+    }
+    document.getElementById(itemQty).value = currentItemQty;
+    document.getElementById(itemPriceTag).innerText = '$' + currentPrice;
 }
 
 document.getElementById('phone-minus').addEventListener('click', function (event) {
-    updateItems('phone-qty', 'unit-price-phone');
+    const unitPricePhone = 1219;
+    updateItems('phone-qty', 'price-phone', unitPricePhone, 'minus');
+
 })
 document.getElementById('phone-plus').addEventListener('click', function (event) {
+    const unitPricePhone = 1219;
+    updateItems('phone-qty', 'price-phone', unitPricePhone, 'plus');
+})
+document.getElementById('case-minus').addEventListener('click', function (event) {
+    const unitPriceCase = 59;
+    updateItems('case-qty', 'price-case', unitPriceCase, 'minus');
 
+})
+document.getElementById('case-plus').addEventListener('click', function (event) {
+    const unitPriceCase = 59;
+    updateItems('case-qty', 'price-case', unitPriceCase, 'plus');
 })
